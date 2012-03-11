469d521c1062ee6da697d8cac1435a6ca0729c40
Jeremy Darby <jdarby@caen-chry-141-212-239-206.wireless.engin.umich.edu>
2012-03-11 14:59:37 -0400

    Adding in cropping option. It should be very close to working if it doesn't work exactly right yet. I'm not exactly sure how to test that it's working properly at the moment.

--- src/eecs/dietary/assistant/DietaryAssistantActivity.java
+++ src/eecs/dietary/assistant/DietaryAssistantActivity.java
@@ -4,10 +4,8 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.View;
 
 
